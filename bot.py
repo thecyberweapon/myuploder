ce48b6812a4194926ebe80ca936f32f6ba969d47
thecyberweapon <78067399+thecyberweapon@users.noreply.github.com>
2022-05-14 16:43:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(958850850)
+    Config.AUTH_USERS.add(5295918462)
     app.run()
