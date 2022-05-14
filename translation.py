70ece178ba14ca1d2b6bb18897857a1bc9b3f4b0
thecyberweapon <78067399+thecyberweapon@users.noreply.github.com>
2022-05-14 16:42:05 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hi {},
-I'm X-URL-Uploader!
+I'm Lucifer-MorningStar!
 You can upload HTTP/HTTPS direct link, Using this bot!
 
 /help for more details!"""
@@ -11,7 +11,7 @@ URL | filename | username | password"""
     UPLOAD_START = "Now Uploading.."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @xurluploaderbot)"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@xurluploaderbot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@LuciferVidBot"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
@@ -31,4 +31,4 @@ If bot didn't respond, contact @xgorn"""
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
     CANCEL_STR = "Process Cancelled"
     ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> http://B34STX.tk and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
